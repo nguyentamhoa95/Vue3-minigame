66a76f24ed544cd4ac7f7f40a443be5d866c3c45
Hoa <tamhoaanhlinh1995@gmail.com>
2022-08-08 11:31:47 +0700

    upload source codee

---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 300) {
+      this.rank = "Great !";
+    } else if (this.score < 400) {
+      this.rank = "Good job !";
+    } else {
+      this.rank = "Try again";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4rem;
+  color: turquoise;
+  font-weight: bold;
+}
+</style>
